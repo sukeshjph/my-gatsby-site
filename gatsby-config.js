c1266ac46c85c8bfbea2132b07f0f1424d3ce8ae
Sukesh Dash <sukesjh@yahoo.com>
2023-03-14 14:25:59 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Sukesh Demo Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
